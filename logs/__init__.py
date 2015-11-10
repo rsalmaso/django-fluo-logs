b458d7459cc21a23b516c886a2c83b6e9ca407ec
Raffaele Salmaso <raffaele@salmaso.org>
2015-11-10 10:29:09 +0100

    release version 0.2.0

--- logs/__init__.py
+++ logs/__init__.py
@@ -24,7 +24,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 from .version import get_version
 
-VERSION = (0, 1, 0, "final", 0)
+VERSION = (0, 2, 0, "final", 0)
 
 __version__  = get_version(VERSION)
 __author__  = "Raffaele Salmaso"
